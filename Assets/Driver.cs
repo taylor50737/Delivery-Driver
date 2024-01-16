a2b3738614f9a69fbc116ec2ffffd65665afff30
Taylor Lo <taylorloph@gmail.com>
2024-01-16 14:09:58 -0500

    Added steerSpeed and moveSpeed variables

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    float steerSpeed = 1;
+    float moveSpeed = 0.01f;
+
     void Start()
     {
     }
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
