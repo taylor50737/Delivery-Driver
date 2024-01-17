c17ff590852f0948d387444d47876ecd472305f4
Taylor Lo <taylorloph@gmail.com>
2024-01-16 19:09:16 -0500

    Added trigger

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Boom");
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("What was that?");
+    }
 }
